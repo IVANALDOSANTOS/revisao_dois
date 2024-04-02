f8c37bcf7621b933e020012b7318be6e17fd5b24
ivanaldo da silva santos <ivanaldo594@gmail.com>
2024-04-02 20:32:31 -0300

    função soma dois numeros

--- entrada_e_saida.py
+++ entrada_e_saida.py
@@ -3,3 +3,11 @@ def saudacao():
     print("Olá, " + nome + "! Bem-vindo!")
 
 saudacao()
+
+def soma():
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    resultado = num1 + num2
+    print("A soma dos números é:", resultado)
+
+soma()
